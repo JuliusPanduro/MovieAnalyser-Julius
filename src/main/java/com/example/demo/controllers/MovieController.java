96cd81075b801de525162d9118dd5896121cfab9
JuliusPanduro <73829015+JuliusPanduro@users.noreply.github.com>
2021-02-15 18:41:33 +0100

    FInal

--- src/main/java/com/example/demo/controllers/MovieController.java
+++ src/main/java/com/example/demo/controllers/MovieController.java
@@ -20,7 +20,6 @@ public class MovieController {
     @GetMapping("/")
     public String frontPage() {
         return "Welcome to the movie analyser web application<br><br>" +
-                "You can now write in your url what you want to see, type things like<br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getFirst\" -> To get the title of the first movie in list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getRandom\" -> To get a random movie title from the list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getTenSortByPopularity\" -> To get the ten most popular movies from the list</a><br><br>" +
@@ -37,7 +36,7 @@ public class MovieController {
         return "Title of the first movie is: " + firstMovieOfList.getTitle() + "<br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getRandom\" -> To get a random movie title from the list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getTenSortByPopularity\" -> To get the ten most popular movies from the list</a><br><br>" +
-                "Click here: " + "<a href=\"http://localhost:8080/getHowManyWonAnAward\" -> To get all the movies from the list that won an award</a><br><br>"+
+                "Click here: " + "<a href=\"http://localhost:8080/getHowManyWonAnAward\" -> To get all the movies from the list that won an award</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/\" -> Return to frontpage</a><br><br>";
     }
 
@@ -48,7 +47,7 @@ public class MovieController {
         return "Title of the random movie is: " + randomMovieFromList.getTitle() + "<br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getFirst\" -> To get the title of the first movie in list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getTenSortByPopularity\" -> To get the ten most popular movies from the list</a><br><br>" +
-                "Click here: " + "<a href=\"http://localhost:8080/getHowManyWonAnAward\" -> To get all the movies from the list that won an award</a><br><br>"+
+                "Click here: " + "<a href=\"http://localhost:8080/getHowManyWonAnAward\" -> To get all the movies from the list that won an award</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/\" -> Return to frontpage</a><br><br>";
     }
 
@@ -64,7 +63,7 @@ public class MovieController {
         return "Ten movie titles, sorted by popularity: <br>" + str + "<br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getFirst\" -> To get the title of the first movie in list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getRandom\" -> To get a random movie title from the list</a><br><br>" +
-                "Click here: " + "<a href=\"http://localhost:8080/getHowManyWonAnAward\" -> To get all the movies from the list that won an award</a><br><br>"+
+                "Click here: " + "<a href=\"http://localhost:8080/getHowManyWonAnAward\" -> To get all the movies from the list that won an award</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/\" -> Return to frontpage</a><br><br>";
 
     }
@@ -75,7 +74,7 @@ public class MovieController {
         return "Number of movies who won an award is: " + movieService.getWonAnAward() + "<br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getFirst\" -> To get the title of the first movie in list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/getRandom\" -> To get a random movie title from the list</a><br><br>" +
-                "Click here: " + "<a href=\"http://localhost:8080/getTenSortByPopularity\" -> To get the ten most popular movies from the list</a><br><br>"+
+                "Click here: " + "<a href=\"http://localhost:8080/getTenSortByPopularity\" -> To get the ten most popular movies from the list</a><br><br>" +
                 "Click here: " + "<a href=\"http://localhost:8080/\" -> Return to frontpage</a><br><br>";
     }
 
